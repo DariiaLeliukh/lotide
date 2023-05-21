9083440de3bb598b415541e27e83418b58ebf43d
DariiaLeliukh <dariialelyukh@gmail.com>
2023-05-20 21:50:50 -0500

    added more tests to tail.js to check empty array and array with only one element

--- tail.js
+++ tail.js
@@ -13,5 +13,8 @@ assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-const result2 = tail([]);
-console.log(tail(result2));
+const result2 = tail([]); //tail of empty array
+console.log(result2);
+
+const result3 = tail(["Hello"]); //tail of 1 element array
+console.log(result3);
