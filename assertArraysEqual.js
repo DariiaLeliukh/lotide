9f2b81ae40f707e4f556cb527e110de67fb1a3df
DariiaLeliukh <dariialelyukh@gmail.com>
2023-05-21 00:43:19 -0500

    added assertArraysEqual.js file [Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.]

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  return actual === expected;
+};
+const eqArrays = function(array1, array2) {
+  let areNotEqual = true;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (!assertEqual(array1[i], array2[i])) {
+        areNotEqual = false;
+      }
+    }
+  }
+  return areNotEqual;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  console.log(`${(eqArrays(array1, array2)) ? "✅✅✅ Assertion Passed: " : "🛑🛑🛑 Assertion Failed: "} ${array1} ${(eqArrays(array1, array2)) ? " === " : " !== "} ${array2}`);
+}
+
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+assertArraysEqual([1, 2, 3], ["3", 2, 1]) // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
