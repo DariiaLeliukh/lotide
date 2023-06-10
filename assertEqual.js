272d8a7f78f4dd431cac2e7e79e19bc2d5c6f378
DariiaLeliukh <dariialelyukh@gmail.com>
2023-06-10 15:20:21 -0500

    moved assertEqual.js and head.js test code to separate test files

--- assertEqual.js
+++ assertEqual.js
@@ -3,8 +3,4 @@ const assertEqual = function(actual, expected) {
   console.log(`${(actual === expected) ? "✅✅✅ Assertion Passed: " : "🛑🛑🛑 Assertion Failed: "} ${actual} ${(actual === expected) ? " === " : " !== "} ${expected}`);
 };
 
-// TEST CODE
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,14 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  console.log(`${(actual === expected) ? "✅✅✅ Assertion Passed: " : "🛑🛑🛑 Assertion Failed: "} ${actual} ${(actual === expected) ? " === " : " !== "} ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+module.exports = head;
