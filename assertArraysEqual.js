ae534ed7242b4a52b254f3d5938d907daa5e6fd5
DariiaLeliukh <dariialelyukh@gmail.com>
2023-06-10 17:17:54 -0500

    refactored assertArraysEqual to move tests to other file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,7 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  return actual === expected;
-};
-const eqArrays = function(array1, array2) {
-  let areEqual = true;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (!assertEqual(array1[i], array2[i])) {
-        areEqual = false;
-      }
-    }
-  } else areEqual = false;
-  return areEqual;
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   console.log(`${(eqArrays(array1, array2)) ? "✅✅✅ Assertion Passed: " : "🛑🛑🛑 Assertion Failed: "} ${array1} ${(eqArrays(array1, array2)) ? " === " : " !== "} ${array2}`);
 };
-
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArraysEqual([1, 2, 3], ["3", 2, 1]); // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
